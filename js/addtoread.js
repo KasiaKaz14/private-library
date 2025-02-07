9b0bfc06f1b7901da1c754ba62bd4627bad2a56f
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2025-02-07 15:12:25 +0100

    read.html finished, addtoread.js functions finished, and works correctly, delete and read btns finished, all btns styled

---
+++ js/addtoread.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const readBookList = document.querySelector('.read-list');
+
+    let readBooks = JSON.parse(localStorage.getItem('readBooks')) || [];
+
+    const renderReadBooks = () => {
+        readBookList.innerHTML = '';
+
+        if (readBooks.length === 0) {
+            const p = document.createElement('p');
+            p.classList.add('read-info');
+            p.innerHTML = 'No books added yet';
+            readBookList.appendChild(p);
+            return;
+        }
+
+        readBooks.forEach((book, index) => {
+            const readBook = document.createElement('div');
+            readBook.classList.add('readbook-list');
+
+            readBook.innerHTML = `
+                <section class="readbook-section">
+                    <section class="cover-img">
+                        <img class="book-cover" src="${book.coverImage}" alt="${book.title}" />
+                    </section>
+                    <section class="read-data">
+                        <h2>${book.title}</h2>
+                        <p><strong>Author:</strong> ${book.name} ${book.surname}</p>
+                        <p><strong>Species:</strong> ${book.species}</p>
+                        <p><strong>Start Date:</strong> ${book.startDate}</p>
+                        <p><strong>End Date:</strong> ${book.endDate}</p>
+                        <p><strong>Description:</strong> ${book.description ? book.description : "No description"}</p>
+                        <button class="delete-btn" data-index="${index}" type="button">Delete book</button>
+                    </section>
+                </section>
+            `;
+
+            readBookList.appendChild(readBook);
+        });
+
+      
+        document.querySelectorAll('.delete-btn').forEach(button => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+                let index = e.target.getAttribute('data-index');
+                readBooks.splice(index, 1);
+                localStorage.setItem("readBooks", JSON.stringify(readBooks));
+                renderReadBooks(); 
+            });
+        });
+    };
+
+    renderReadBooks(); 
+});
--- js/display-books.js
+++ js/display-books.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const bookList = document.querySelector('.book-list');
 
+
     let books = JSON.parse(localStorage.getItem("books")) || [];
+    let readBooks = JSON.parse(localStorage.getItem("readBooks")) || [];
 
     if (books.length === 0) {
         const p = document.createElement('p');
@@ -27,6 +29,8 @@ document.addEventListener("DOMContentLoaded", function () {
         books.forEach((book, index) => {
             const bookItem = document.createElement('div');
             bookItem.classList.add('book-item');
+const isRead = readBooks.some((readBook) => readBook.title === book.title);
+
             bookItem.innerHTML = `
             <section class="book">
             <section class="cover-img">
@@ -39,7 +43,10 @@ document.addEventListener("DOMContentLoaded", function () {
             <p><strong>Start Date:</strong> ${book.startDate}</p>
             <p><strong>End Date:</strong> ${book.endDate ? book.endDate : "Not finished yet"}</p>
             <p><strong>Description:</strong> ${book.description ? book.description : "No description"}</p>
+            <section class="button">
             <button type="button" class="delete-btn" data-index="${index}">Delete book</button>
+             <button type="button" class="read-btn" data-index="${index}" ${isRead} ? disable : " ">${isRead ? "Added to read" : "Add to read"}</button>
+            </section>
             </section>
             </section>
             `;
@@ -54,6 +61,22 @@ document.addEventListener("DOMContentLoaded", function () {
                 renderBooks();
             });
         });
+
+        document.querySelectorAll('.read-btn').forEach(button => {
+            button.addEventListener('click', (e) => {
+
+                let index = e.target.getAttribute("data-index");
+
+                if(!readBooks.some((readBook) => readBook.title === books[index].title)){
+                    books[index].endDate = new Date().toISOString().split('T')[0];
+                    readBooks.push(books[index]);
+                    localStorage.setItem('readBooks', JSON.stringify(readBooks));
+                    renderBooks();
+                }
+
+            })
+        })
+
     };
 
     renderBooks(); 
